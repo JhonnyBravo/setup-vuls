a91d2bfd02c91bf64a2430295b8be02bd7ef1d41
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-01-04 15:47:55 +0900

    modified: cookbooks/go/install_package.rb
    get_package の発動条件を修正。
    install_package の発動条件を修正。

--- cookbooks/go/install_package.rb
+++ cookbooks/go/install_package.rb
@@ -5,12 +5,12 @@ VERSION=ENV["VERSION"]
 OS=ENV["OS"]
 
 execute "get_package" do
-  not_if "test -f go#{VERSION}.#{OS}.tar.gz"
+  not_if "test -d /usr/local/go"
   command "wget https://redirector.gvt1.com/edgedl/go/go#{VERSION}.#{OS}.tar.gz"
 end
 
 execute "install_package" do
-  not_if "test -d /usr/loca/go"
+  not_if "test -d /usr/local/go"
   command "tar -C /usr/local -xzf go#{VERSION}.#{OS}.tar.gz"
 end
 
