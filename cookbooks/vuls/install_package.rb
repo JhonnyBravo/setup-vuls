e5a02f7653202dc1ea1277e3aa11c6f4f0ef7c98
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-04-16 03:33:10 +0900

    modified: cookbooks/vuls/install_package.rb
    raspbian stretch へのバージョンアップに伴い、不要になった処理を削除。

--- cookbooks/vuls/install_package.rb
+++ cookbooks/vuls/install_package.rb
@@ -1,24 +1,12 @@
-APT_SOURCE="deb http://httpredir.debian.org/debian jessie-backports main"
-
 directory "/var/log/vuls" do
   action :create
   owner "vuls"
   mode "700"
 end
 
-execute "add_apt_source" do
-  not_if "grep -a 'jessie-backports' /etc/apt/sources.list"
-  command "echo #{APT_SOURCE} >> /etc/apt/sources.list"
-end
-
-execute "apt_get_update" do
-  command "apt-get update"
-end
-
 %w(git make gcc reboot-notifier).each do |pkg|
   package pkg do
     action :install
-    options "--force-yes"
   end
 end
 
